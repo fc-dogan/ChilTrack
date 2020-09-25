cab775b0abd384c66d7252062da25eb53cfde56e
Fatma <fceyda@gmail.com>
2020-09-25 16:25:15 -0700

    update CreateNewKidProfile component to  adding data to firestore

--- src/Components/CreateNewKidProfile.js
+++ src/Components/CreateNewKidProfile.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useFirestore } from 'react-redux-firebase';
 
 function CreateNewKidProfile() {
+
+  const firestore = useFirestore();
+
+  function addEventToFirestore(event){
+    event.preventDefault();
+
+    return firestore.collection('kids').add(
+      {
+        name: event.target.name.value,
+        totalPoint: 0,
+        createdTime: firestore.FieldValue.serverTimestamp()
+      }
+    );
+  }
+
   return (
-    <div>
-      <p>create kid profile form</p>
+    <div className="container">
+    <form className="white" onSubmit={addEventToFirestore}>
+      <h5 className="grey-text text-darken-3">Add a new Kid Profile</h5>
+      <div className="input-field">
+        <input type="text" name='name' />
+        <label htmlFor="name">Name</label>
+      </div>
+      <div className="input-field">
+        <button className="btn pink lighten-1">Create</button>
+      </div>
+    </form>
     </div>
   )
 }
 
 export default CreateNewKidProfile
+
--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -1,12 +1,20 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import kidListReducer from '../../reducers/kidList-reducer';
 
 describe("rootReducer", () => {
+  let store = createStore(rootReducer)
 
-  test('Should return default state if no action type is recognized', () => {
-    expect(rootReducer({}, { type: null })).toEqual({
-      kidList: {},
-      auth: {}
-    });
+  test('Check that initial state of kidListReducer matches root reducer', () => {
+    expect(store.getState().kidList).toEqual(kidListReducer({}, { type: null }))
   });
+
+  // test('Should return default state if no action type is recognized', () => {
+  //   expect(rootReducer({}, { type: null })).toEqual({
+  //     kidList: {},
+  //     auth: {},
+  //     firestore: {}
+  //   });
+  // });
 
 });

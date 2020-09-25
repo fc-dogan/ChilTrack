4c70252c461c328141e694740ff7ae79c70cfdd5
Fatma <fceyda@gmail.com>
2020-09-25 15:11:08 -0700

    create redux store index.js

--- src/reducers/kidList-reducer.js
+++ src/reducers/kidList-reducer.js
@@ -1,4 +1,10 @@
-const kidListReducer = (state ={}, action)=>{
+// const initialState ={
+//   kidList: [
+//     {id:'1', name:'Ali', },
+//     {id:'2', name:'Nevra', }
+//   ]
+// }
+const kidListReducer = (state = {}, action)=>{
   return state;
 }
 export default kidListReducer;

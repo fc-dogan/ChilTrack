edd3d05370bcc5f9178d7271479ecec51c59b373
Fatma <fceyda@gmail.com>
2020-09-25 10:26:58 -0700

    create navBar component

---
+++ src/Components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Navbar() {
+  return (
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <h1>ChilTrack</h1>
+      
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar

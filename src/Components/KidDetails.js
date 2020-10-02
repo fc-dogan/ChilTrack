3a8ba9028a9a0dccbbe42fcff6b9d8e34baf475a
Fatma <fceyda@gmail.com>
2020-10-02 10:26:24 -0700

    add deleting kidsprofile function and redirect to dashboard

--- src/Components/KidDetails.js
+++ src/Components/KidDetails.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+import { useFirestore} from 'react-redux-firebase';
+import { useHistory } from 'react-router-dom';
 
 function KidDetails(props) {
-
-
+  const history = useHistory()
   const id = props.location.props.id
   const kids = useSelector(state => state.firestore.data.kids);
   const kid = kids[id]
 
  console.log('kid ' + kid.name)
-
+ const firestore = useFirestore();
+  
+ const handleDeletingKidsProfile =() =>{
+    firestore.delete({collection: 'kids', doc: id });
+    history.push('/dashboard')
+ }
   return (
     <div className="container section kid-details">
     <div className="card z-depth-0">
@@ -20,6 +25,9 @@ function KidDetails(props) {
       <div className="card-action grey lighten-4 grey-text">
         <div>Total Point {kid.totalPoint} </div>
       </div>
+    <div className="card-footer">
+      <button onClick={handleDeletingKidsProfile}>Delete </button>
+    </div>
     </div>
   </div>
   )

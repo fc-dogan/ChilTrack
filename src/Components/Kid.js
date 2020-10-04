9d5f0aedce3133ceb6a2649099161bccd0f9ee61
Fatma <fceyda@gmail.com>
2020-10-03 20:10:05 -0700

    add close icon for decrement points

--- src/Components/Kid.js
+++ src/Components/Kid.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import {incrementKidsPoints, decrementKidsPoints } from '../actions/kidsActions'
 import { Icon } from 'react-icons-kit'
-import {checkmark} from 'react-icons-kit/icomoon/checkmark'
+import {checkmark } from 'react-icons-kit/icomoon/checkmark'
+import {close} from 'react-icons-kit/ionicons/close'
 
 function Kid(props) {
   const dispatch = useDispatch();
@@ -17,24 +18,26 @@ function Kid(props) {
   }
     return (
       <React.Fragment>
+        <div className="card z-depth-0 ">
 
       <Link to={{
         pathname:`/details/${props.id}`,
         props: { id: props.id}
         }} key={props.id}>  
-          <div className="card z-depth-0 ">
           <div className="card-action grey lighten-4 grey-text">
             <p>Name: {props.name} </p>
             <p>Total Point: <em>{props.totalPoint}</em></p>
           </div>
-          </div>
       </Link>
           <div className="card-action ">
             <button onClick={increasePoint} style={{ color: '#F4A261' }}>
-              <Icon size={45} icon={checkmark}/>
+              <Icon size={30} icon={checkmark}/>
             </button>
             {/* <button onClick={increasePoint}>increase point</button> */}
-            <button onClick={decreasePoint}>decrease point</button>
+            <button onClick={decreasePoint} style={{ color: '#F4A261' }}>
+              <Icon size={30} icon={close} />
+            </button>
+          </div>
           </div>
       </React.Fragment>
     ) 
